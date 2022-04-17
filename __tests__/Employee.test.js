a70d15ae84ec038cdec88c87a60b268189f75022
Steffen568 <sgonzalez568@gmail.com>
2022-04-17 15:41:47 -0500

    added passing test for Employee

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,30 @@
+const Employee = require('../lib/Employee')
+
+it(`Creates an Employee object with name, id, and email`, () => {
+    const employee = new Employee(`stef`, `789`, `test@gmail.com`)
+
+    expect(employee.name).toEqual(`stef`)
+    expect(employee.id).toEqual(`789`)
+    expect(employee.email).toEqual(`test@gmail.com`)
+})
+
+it(`Retutns employee object`, () => {
+    const employee = new Employee(`stef`, `789`, `test@gmail.com`)
+    expect(employee.genPosition()).toEqual(Employee)
+})
+
+it(`Returns the name of an Employee`, () => {
+    const employee = new Employee(`stef`, `789`, `test@gmail.com`)
+    expect(employee.name).toEqual(`stef`)
+})
+
+it(`Returns id of an employee`, () => {
+    const employee = new Employee(`stef`, `789`, `test@gmail.com`)
+    expect(employee.id).toEqual(`789`)
+})
+
+it(`Returns email of an employee`, () => {
+    const employee = new Employee(`stef`, `789`, `test@gmail.com`)
+    expect(employee.email).toEqual(`test@gmail.com`)
+})
+
